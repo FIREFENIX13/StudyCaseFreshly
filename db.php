b034e22962a73b23e09c49e4dc28c297e20e98d4
FIREFENIX13 <firefenix13@hotmail.com>
2025-03-24 20:18:23 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,1 @@
-<?php
-// db.php
-$host = 'localhost';
-$user = 'freshlytest';
-$pass = '';
-$dbname = 'freshlytest';
 
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-?>
